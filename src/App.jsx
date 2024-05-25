8df7b7dd124de8ba06cfaae3892b0a96445877c1
nawabkhan89646 <nkhan89646@gmail.com>
2024-05-25 21:36:07 +0530

    adding day2 classwork

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='content'>
+      <h1>Learn Web Development</h1>
+      <p>Welcome to the MDN learning area. This set of article aims to provide complete beginners to web development with all that they need to start coding websites.</p>
+      <p>The aim of this area of MDN is not to take you from "beginner" to "espert" but to take you from "beginner" to "comfortable". From there, you should be able to start making your way, learning from the <a href="https://developer.mozilla.org/en-US/" target='_blank'>rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge.</p>
+      <p>If you are a complete beginner, web development can be challenging - we will hold your hand and provide enough details for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.</p>
+    </div>
+  )
+}
+
+export default App
